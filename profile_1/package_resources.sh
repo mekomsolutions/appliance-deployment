c4144adbac6c828ae7fcaf932f80fcdb94882d71
rbuisson <romain@mekomsolutions.com>
2021-04-26 09:19:01 +0200

    INFRA-240: Uncomment lines

--- profile_1/package_resources.sh
+++ profile_1/package_resources.sh
@@ -14,14 +14,14 @@ mkdir -p $BUILD_DIR
 
 # Fetch distro
 echo "⚙️ Download $DISTRO_NAME distro..."
-#wget https://nexus.mekomsolutions.net/repository/maven-snapshots/net/mekomsolutions/bahmni-distro-$DISTRO_NAME/$DISTRO_VERSION/bahmni-distro-$DISTRO_NAME-$DISTRO_REVISION.zip -O $BUILD_DIR/bahmni-distro-c2c.zip
-# mkdir -p $RESOURCES_DIR/distro
-# unzip $BUILD_DIR/bahmni-distro-c2c.zip -d $RESOURCES_DIR/distro
+wget https://nexus.mekomsolutions.net/repository/maven-snapshots/net/mekomsolutions/bahmni-distro-$DISTRO_NAME/$DISTRO_VERSION/bahmni-distro-$DISTRO_NAME-$DISTRO_REVISION.zip -O $BUILD_DIR/bahmni-distro-c2c.zip
+mkdir -p $RESOURCES_DIR/distro
+unzip $BUILD_DIR/bahmni-distro-c2c.zip -d $RESOURCES_DIR/distro
 
 # Fetch K8s files
 echo "⚙️ Fetch K8s description files..."
-# rm -rf ./$BUILD_DIR/k8s-description-files
-# git clone https://github.com/mekomsolutions/k8s-description-files.git $BUILD_DIR/k8s-description-files
+rm -rf ./$BUILD_DIR/k8s-description-files
+git clone https://github.com/mekomsolutions/k8s-description-files.git $BUILD_DIR/k8s-description-files
 
 echo "⚙️ Run Helm to substitute custom values..."
 helm template `[ -f $DISTRO_VALUES_FILE ] && echo "-f $DISTRO_VALUES_FILE"` `[ -f $DEPLOYMENT_VALUES_FILE ] && echo "-f $DEPLOYMENT_VALUES_FILE"` $DISTRO_NAME ./$BUILD_DIR/k8s-description-files/src/bahmni-helm --output-dir $RESOURCES_DIR/k8s
