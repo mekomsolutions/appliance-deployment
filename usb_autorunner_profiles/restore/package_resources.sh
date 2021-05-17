3a06a7b5e72ce437e3d43bc5a91026879f05a275
rbuisson <romain@mekomsolutions.com>
2021-05-17 18:23:43 +0200

    INFRA-287: Add 'restore' profile

---
+++ usb_autorunner_profiles/restore/package_resources.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+PWD=$(dirname "$0")
+RESOURCES_DIR=$PWD/target/resources
+FILE=$PWD/dump.sql
+
+rm -rf $RESOURCES_DIR
+mkdir -p $RESOURCES_DIR
+
+if [[ -f "$FILE" ]]; then
+    echo "$FILE exists."
+else
+  echo "⚠️ $FILE is missing. Please drop the 'dump.sql' file in $PWD/ folder"
+  echo "🚫 Abort."
+  exit 1
+fi
+
+echo "⚙️  Copy 'dump.sql'"
+cp $PWD/dump.sql $RESOURCES_DIR/
+
+echo "⚙️  Copy 'run.sh'"
+cp -R $PWD/run.sh $RESOURCES_DIR/
---
+++ usb_autorunner_profiles/restore/run.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+set -e
+
+kubectl="/usr/local/bin/k3s kubectl"
+PWD=$(dirname "$0")
+REGISTRY_IP=10.0.90.99
+NAMESPACE=""
+
+JOB_NAME=openmrs-db-restore
+OPENMRS_SERVICE_NAME=openmrs
+ARCHIVE_PATH=/opt/autorunner/workdir/dump.sql
+
+mysql_app=`$kubectl get statefulsets.apps | grep 'mysql'`
+if [ -n "$mysql_app" ] ; then
+  echo "⚙️  Fetch MySQL credentials"
+  DB_USERNAME=`$kubectl get configmap mysql-configs -o json | jq '.data.MYSQL_ROOT_USER' | tr -d '"'`
+  DB_PASSWORD=`$kubectl get configmap mysql-configs -o json | jq '.data.MYSQL_ROOT_PASSWORD' | tr -d '"'`
+  DB_NAME=openmrs
+
+  echo "Remove previous job, if exists"
+  $kubectl delete --ignore-not-found=true job ${JOB_NAME}
+
+  echo "Stop the OpenMRS service"
+  $kubectl scale deployment ${OPENMRS_SERVICE_NAME} --replicas 0
+
+  echo "⚙️  Run MySQL restore job"
+  cat <<EOF | $kubectl apply -f -
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: "${JOB_NAME}"
+  labels:
+    app: db-restore
+spec:
+  template:
+    spec:
+      affinity:
+        nodeAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+            nodeSelectorTerms:
+            - matchExpressions:
+              - key: role
+                operator: In
+                values:
+                - database
+      volumes:
+      - name: restore-storage
+        hostPath:
+          path: ${ARCHIVE_PATH}
+          type: File
+      containers:
+      - name: mysql-db-restore
+        image: 10.0.90.99/mekomsolutions/mysql_backup:9ab7a24
+        command: ["mysql"]
+        args: ["-hmysql", "-u${DB_USERNAME}", "-p${DB_PASSWORD}", "${DB_NAME}", "-e", "source /opt/dump.sql"]
+        env:
+        volumeMounts:
+        - name: restore-storage
+          mountPath: /opt/dump.sql
+      restartPolicy: Never
+EOF
+  echo "🕐 Wait for the job to complete... (timeou=1h)"
+  $kubectl wait --for=condition=complete --timeout 3600s job/${JOB_NAME}
+  echo "Completed."
+
+  echo "Set OpenMRS index to be rebuilt"
+
+  echo "🚀 Restart OpenMRS service"
+  $kubectl scale deployment ${OPENMRS_SERVICE_NAME} --replicas 1
+
+else
+  echo "⚠️  MySQL service is not found, abort"
+  exit 1
+fi
+echo "✅ Done."
