21aada86d0f7cfa5a3016787c236325c7610928c
Zoheir CHINE <chine.zoheir@gmail.com>
2021-08-11 22:55:42 +0800

    INFRA-285: Fix backup profile (#6)

--- usb_autorunner_profiles/backup/run.sh
+++ usb_autorunner_profiles/backup/run.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
-set -e
+
+DISTRO_NAME=c2c
 kubectl="/usr/local/bin/k3s kubectl"
 # Get NFS IP address
 registry_ip=`$kubectl get svc registry-service -o json | jq '.spec.loadBalancerIP' | tr -d '"'`
 # Get USB mount point
 usb_mount_point=`grep "mount_point" /opt/autorunner/usbinfo | cut -d'=' -f2 | tr -d '"'`
-echo "ℹ️ Archives will be saved in '${usb_mount_point}'"
+backup_folder=${usb_mount_point}/backup
+echo "ℹ️ Archives will be saved in '${backup_folder}'"
+mkdir -p $backup_folder
+logs_folder=/mnt/disks/ssd1/logging
 
 echo "⚙️  Delete old backup jobs"
 $kubectl delete job -l app=usb-backup --ignore-not-found=true
+$kubectl delete job -n rsyslog -l app=usb-backup --ignore-not-found=true
 
+mkdir -p ${backup_folder}/filestore
 echo "⚙️  Run Filestore backup job"
 # Backup filestore
 cat <<EOF | $kubectl apply -f -
@@ -28,10 +34,10 @@ spec:
             claimName: data-pvc
         - name: backup-path
           hostPath:
-            path: "${usb_mount_point}"
+            path: "${backup_folder}/filestore"
       containers:
       - name: data-backup
-        image: ${registry_ip}/mekomsolutions/filestore_backup:latest
+        image: ${registry_ip}/mekomsolutions/filestore_backup:9ab7a24
         env:
           - name: FILESTORE_PATH
             value: /opt/data
@@ -42,26 +48,24 @@ spec:
         - name: backup-path
           mountPath: /opt/backup
       restartPolicy: Never
+      nodeSelector:
+        role: database
 EOF
 
-mysql_app=`$kubectl get statefulsets.apps | grep 'mysql'`
-if [ -n "$mysql_app" ] ; then
-  echo "⚙️ Fetch MySQL credentials"
-  mysql_root_user=`$kubectl get configmap mysql-configs -o json | jq '.data.MYSQL_ROOT_USER' | tr -d '"'`
-  mysql_root_password=`$kubectl get configmap mysql-configs -o json | jq '.data.MYSQL_ROOT_PASSWORD' | tr -d '"'`
-  mysql_databases=`$kubectl exec -ti mysql-0 -- mysql -u $mysql_root_user -p$mysql_root_password -BNe "show databases;"`
 
-  echo "⚙️ Run MySQL backup job"
-  # Backup MySQL Databases
-  for database in $mysql_databases
-  do
-    echo "Backing up '${database}'..."
-    hash=`cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 7 | head -n 1`
-    cat <<EOF | $kubectl apply -f -
+echo "⚙️ Fetch MySQL credentials"
+mysql_root_user=`$kubectl get configmap mysql-configs -o json | jq '.data.MYSQL_ROOT_USER' | tr -d '"'`
+mysql_root_password=`$kubectl get configmap mysql-configs -o json | jq '.data.MYSQL_ROOT_PASSWORD' | tr -d '"'`
+
+echo "⚙️ Run MySQL backup job"
+# Backup MySQL Databases
+
+echo "Backing up OpenMRS database"
+cat <<EOF | $kubectl apply -f -
 apiVersion: batch/v1
 kind: Job
 metadata:
-  name: mysql-$hash-db-backup
+  name: mysql-openmrs-db-backup
   labels:
     app: usb-backup
 spec:
@@ -73,13 +77,13 @@ spec:
             claimName: data-pvc
         - name: backup-path
           hostPath:
-            path: "${usb_mount_point}/mysql"
+            path: "${backup_folder}"
       containers:
       - name: mysql-db-backup
-        image: ${registry_ip}/mekomsolutions/mysql_backup:latest
+        image: ${registry_ip}/mekomsolutions/mysql_backup:9ab7a24
         env:
           - name: DB_NAME
-            value: ${database}
+            value: openmrs
           - name: DB_USERNAME
             value: ${mysql_root_user}
           - name: DB_PASSWORD
@@ -90,31 +94,30 @@ spec:
         - name: backup-path
           mountPath: /opt/backup
       restartPolicy: Never
+      nodeSelector:
+        role: database
 EOF
-  done
-else
-  echo "ℹ️ MySQL service is not found, skipping."
-fi
 
-postgres_app=`$kubectl get statefulsets.apps | grep 'postgres'`
-if [ -n "$postgres_app" ] ; then
-  # Backup PostgreSQL databases
-  echo "⚙️ Fetch PostgreSQL credentials and database names"
-  postgres_user=`$kubectl get configmap postgres-configs -o json | jq '.data.POSTGRES_USER' | tr -d '"'`
-  postgres_password=`$kubectl get configmap postgres-configs -o json | jq '.data.POSTGRES_PASSWORD' | tr -d '"'`
-  postgres_databases=`kubectl exec -ti postgres-0 -- psql -n -U $postgres_user --pset="footer=off" --pset="border=0" --pset="tuples_only=on" -c "SELECT datname FROM pg_database;"`
+# Backup PostgreSQL databases
+echo "⚙️ Fetch Odoo database credentials"
+odoo_user=`$kubectl get configmap odoo-configs -o json | jq '.data.ODOO_DB_USER' | tr -d '"'`
+odoo_password=`$kubectl get configmap odoo-configs -o json | jq '.data.ODOO_DB_PASSWORD' | tr -d '"'`
+odoo_database=`$kubectl get configmap odoo-configs -o json | jq '.data.ODOO_DB_NAME' | tr -d '"'`
 
-  echo "⚙️ Run PostgreSQL backup jobs"
-  # Backup PostgreSQL Databases
-  for database in $postgres_databases
-  do
-    echo "Backing up '${database}'..."
-    hash=`cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 7 | head -n 1`
-    cat <<EOF | $kubectl apply -f -
+echo "⚙️ Fetch OpenELIS database credentials"
+openelis_user=`$kubectl get configmap openelis-db-config -o json | jq '.data.OPENELIS_DB_USER' | tr -d '"'`
+openelis_password=`$kubectl get configmap openelis-db-config -o json | jq '.data.OPENELIS_DB_PASSWORD' | tr -d '"'`
+openelis_database=`$kubectl get configmap openelis-db-config -o json | jq '.data.OPENELIS_DB_NAME' | tr -d '"'`
+
+
+echo "⚙️ Run PostgreSQL backup jobs"
+# Backup PostgreSQL Databases
+echo "Backing up 'Odoo' database..."
+cat <<EOF | $kubectl apply -f -
 apiVersion: batch/v1
 kind: Job
 metadata:
-  name: postgres-$hash-db-backup
+  name: odoo-db-backup
   labels:
     app: usb-backup
 spec:
@@ -123,26 +126,99 @@ spec:
       volumes:
         - name: backup-path
           hostPath:
-            path: "${usb_mount_point}/postgres"
+            path: "${backup_folder}"
       containers:
       - name: postgres-db-backup
-        image: ${registry_ip}/mekomsolutions/postgres_backup:latest
+        image: ${registry_ip}/mekomsolutions/postgres_backup:9ab7a24
         env:
           - name: DB_HOST
             value: postgres
           - name: DB_NAME
-            value: ${database}
+            value: ${odoo_database}
           - name: DB_USERNAME
-            value: ${postgres_user}
+            value: ${odoo_user}
           - name: DB_PASSWORD
-            value: ${postgres_password}
+            value: ${odoo_password}
         volumeMounts:
         - name: backup-path
           mountPath: /opt/backup
       restartPolicy: Never
+      nodeSelector:
+        role: database
 EOF
-  done
-else
-  echo "ℹ️ PostgreSQL service is not found, skipping."
-fi
+
+cat <<EOF | $kubectl apply -f -
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: openelis-db-backup
+  labels:
+    app: usb-backup
+spec:
+  template:
+    spec:
+      volumes:
+        - name: backup-path
+          hostPath:
+            path: "${backup_folder}"
+      containers:
+      - name: postgres-db-backup
+        image: ${registry_ip}/mekomsolutions/postgres_backup:9ab7a24
+        env:
+          - name: DB_HOST
+            value: postgres
+          - name: DB_NAME
+            value: ${openelis_database}
+          - name: DB_USERNAME
+            value: ${openelis_user}
+          - name: DB_PASSWORD
+            value: ${openelis_password}
+        volumeMounts:
+        - name: backup-path
+          mountPath: /opt/backup
+      restartPolicy: Never
+      nodeSelector:
+        role: database
+EOF
+
+echo "⚙️  Run logs backup job"
+mkdir -p ${backup_folder}/logging
+# Backup filestore
+cat <<EOF | $kubectl apply -n rsyslog -f -
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: logging-data-backup
+  labels:
+    app: usb-backup
+spec:
+  template:
+    spec:
+      volumes:
+        - name: data
+          persistentVolumeClaim:
+            claimName: logging-pvc
+        - name: backup-path
+          hostPath:
+            path: "${backup_folder}/logging"
+      containers:
+      - name: data-backup
+        image: ${registry_ip}/mekomsolutions/filestore_backup:9ab7a24
+        env:
+          - name: FILESTORE_PATH
+            value: /opt/data
+        volumeMounts:
+        - name: data
+          mountPath: "/opt/data"
+          subPath: "./"
+        - name: backup-path
+          mountPath: /opt/backup
+      restartPolicy: Never
+      nodeSelector:
+        role: database
+EOF
+
+echo "⏱ Wait for backup to be ready"
+sleep 3600
+
 echo "✅ Done."
