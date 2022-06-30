cc4761750132e599cee9dc3ebed6c1a0280486d9
Zoheir CHINE <zoheir@mekomsolutions.com>
2022-06-30 12:01:25 +0300

    C2C-138: Various fixes on the restore profile (#19)

--- usb_autorunner_profiles/deploy/run.sh
+++ usb_autorunner_profiles/deploy/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PWD=$(dirname "$0")
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 DISTRO_NAME=c2c
 REGISTRY_IP=${REGISTRY_IP:-10.0.90.99}
 SSD_MOUNT_POINT=/mnt/disks/ssd1/
@@ -30,15 +30,15 @@ $kubectl_bin wait --for=condition=ready --timeout 1800s pod $POD_NAME -n $NAMESP
 
 # sync images to registry
 echo "⚙️  Upload container images to the registry at $REGISTRY_IP..."
-skopeo sync --scoped --dest-tls-verify=false --src dir --dest docker $PWD/images/docker.io $REGISTRY_IP
+cd $SCRIPT_DIR/images/docker.io && skopeo sync --scoped --dest-tls-verify=false --src dir --dest docker ./ $REGISTRY_IP
 
 # Apply config
 echo "⚙️  Apply K8s description files: config/ ..."
-$kubectl_bin apply -f $PWD/k8s/bahmni-helm/templates/configs
+$kubectl_bin apply -f $SCRIPT_DIR/k8s/bahmni-helm/templates/configs
 
 echo "⚙️  Upload the distro..."
 # Sending distro to volume
-$PWD/utils/upload-files.sh $REGISTRY_IP/mdlh/alpine-rsync:3.11-3.1-1 $PWD/distro/ distro-pvc
+$SCRIPT_DIR/utils/upload-files.sh $REGISTRY_IP/mdlh/alpine-rsync:3.11-3.1-1 $SCRIPT_DIR/distro/ distro-pvc
 
 echo "🧽 Delete the current 'openmrs' pod"
 $kubectl_bin delete pods -l app=openmrs -n $NAMESPACE
@@ -51,8 +51,8 @@ $kubectl_bin delete pods -l app=openelis -n $NAMESPACE
 
 # Apply K8s description files
 echo "⚙️  Apply K8s description files: common/ ..."
-$kubectl_bin apply -f $PWD/k8s/bahmni-helm/templates/common
+$kubectl_bin apply -f $SCRIPT_DIR/k8s/bahmni-helm/templates/common
 echo "⚙️  Apply K8s description files: apps/ ..."
-$kubectl_bin apply -f $PWD/k8s/bahmni-helm/templates/apps/ -R
+$kubectl_bin apply -f $SCRIPT_DIR/k8s/bahmni-helm/templates/apps/ -R
 
 echo "✅  Done."
