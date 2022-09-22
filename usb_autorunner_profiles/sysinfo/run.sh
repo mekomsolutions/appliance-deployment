26fe2bd8e758da4d8cc29909c828383ec89d8bd7
Zoheir CHINE <zoheir@mekomsolutions.com>
2022-09-22 15:46:06 +0000

    Fix 'usbinfo' path

--- usb_autorunner_profiles/sysinfo/run.sh
+++ usb_autorunner_profiles/sysinfo/run.sh
@@ -4,7 +4,7 @@ kubectl="/usr/local/bin/k3s kubectl"
 # Get NFS IP address
 registry_ip=`$kubectl get svc registry-service -n appliance -o custom-columns=:.spec.loadBalancerIP --no-headers`
 # Get USB mount point
-usb_mount_point=`grep "mount_point" /opt/autorunner/usbinfo | cut -d'=' -f2 | tr -d '"'`
+usb_mount_point=`grep "mount_point" /etc/usb-autorunner/usbinfo | cut -d'=' -f2 | tr -d '"'`
 echo "ℹ️ Archives will be saved in '${usb_mount_point}'"
 sysinfo_folder=${usb_mount_point}/sysinfo
 mkdir -p ${sysinfo_folder}
@@ -66,4 +66,6 @@ k3s crictl pods | tee ${sysinfo_folder}/crictl_pods.txt
 $kubectl top node | tee ${sysinfo_folder}/nodes_usage.txt > /dev/null
 $kubectl top pod | tee ${sysinfo_folder}/pods_usage.txt > /dev/null
 $kubectl logs $($kubectl get pod -l app=eip-client -o name) | tee ${sysinfo_folder}/eip_client_log.txt > /dev/null
+$kubectl logs $($kubectl get pod -l app=openmrs -o name) | tee ${sysinfo_folder}/openmrs.txt > /dev/null
+$kubectl logs $($kubectl get pod -l app=odoo -o name) | tee ${sysinfo_folder}/eip_client_log.txt > /dev/null
 echo "✅ Done."
