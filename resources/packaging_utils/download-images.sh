2252567e07097f36c2a8699dd581f1f7570b267a
Zoheir CHINE <zoheir@mekomsolutions.com>
2022-12-19 09:34:24 +0100

    C2C-149: Add 'analytics' profile (#31)

--- resources/packaging_utils/download-images.sh
+++ resources/packaging_utils/download-images.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 mkdir -p $1
+set -e
 if [ -p /dev/stdin ]; then
-        while IFS= read line; do
-                skopeo sync --src docker --dest dir --scoped ${line} $1 --override-arch arm64 --override-os linux
-        done
-        if [ -d "$1/docker.io/library" ]; then
-                # library repositories should be moved to the "docker.io" directory root
-                mv $1/docker.io/library/* $1/docker.io
-                rmdir $1/docker.io/library/
+        args=()
+        args+=( '--src' 'docker' '--dest' 'dir' '--override-arch' 'arm64' '--override-os' 'linux' );
+        if [ ! -z ${REGISTRY_USER} ]; then 
+                args+=("--src-creds" "${REGISTRY_USER}:${REGISTRY_PASSWORD}" );
         fi
-
+        echo ${args[@]}
+        while IFS= read line; do
+                skopeo sync "${args[@]}" --scoped ${line} $1
+        done
+        
 else
         echo "No input was found on stdin, skipping!"
         # Checking to ensure a filename was specified and that it exists
---
+++ usb_autorunner_profiles/analytics/package_resources.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Exist if command return non-zero status.
+set -e
+
+DISTRO_VERSION=${DISTRO_VERSION}
+ARTIFACT_GROUP=${ARTIFACT_GROUP:-net.mekomsolutions}
+
+PVC_MOUNTER_IMAGE=mdlh/alpine-rsync:3.11-3.1-1
+
+BASE_DIR=$(dirname "$0")
+BUILD_DIR=$BASE_DIR/target/build
+RESOURCES_DIR=$BASE_DIR/target/resources
+PACKAGING_UTILS_DIR=$PWD/resources/packaging_utils
+IMAGES_FILE=$BUILD_DIR/images.txt
+
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+
+rm -rf $RESOURCES_DIR
+mkdir -p $RESOURCES_DIR
+
+echo "⚙️ Run Helm to substitute custom values..."
+git clone git@github.com:ozone-his/ozone-analytics-helm.git $BUILD_DIR/ozone-analytics
+dir1=$BASE_DIR
+dir2=$PWD
+helm template -f $BASE_DIR/analytics-values.yml analytics $BUILD_DIR/ozone-analytics --output-dir $RESOURCES_DIR/k8s --namespace analytics
+
+# Get container images
+cat /dev/null > $IMAGES_FILE
+echo "⚙️ Add the $PVC_MOUNTER_IMAGE image"
+echo "docker.io/$PVC_MOUNTER_IMAGE" >> $IMAGES_FILE
+
+echo "⚙️ Parse the list of container images..."
+grep -ri "image:" $RESOURCES_DIR/k8s  | awk -F': ' '{print $3}' | xargs | tr " " "\n" >> $IMAGES_FILE
+
+temp_file=$(mktemp)
+cp $IMAGES_FILE $temp_file
+echo "⚙️ Remove duplicates..."
+sort $temp_file | uniq > $IMAGES_FILE
+rm ${temp_file}
+echo "ℹ️ Images to be downloaded:"
+cat $IMAGES_FILE
+
+echo "🚀 Download container images..."
+mkdir -p $BUILD_DIR/images
+cat $IMAGES_FILE | $PACKAGING_UTILS_DIR/download-images.sh $BUILD_DIR/images
+
+# Copy resources
+echo "⚙️ Copy 'run.sh' and 'utils/'..."
+cp -R $BASE_DIR/run.sh $BASE_DIR/manifests $BASE_DIR/utils $BUILD_DIR/images $RESOURCES_DIR/
